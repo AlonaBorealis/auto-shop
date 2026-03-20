4f921685bfd32e26a099147b44b380694943d2fb
Alona Borealis <nadiadmitrov@gmail.com>
2026-03-20 19:28:05 +0300

    categories ready

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const categoriesBtn = document.getElementById('categoriesBtn');
+const categories = document.getElementById('categories');
+const body = document.querySelector('body');
+
+categoriesBtn.addEventListener('click', () => {
+	categories.classList.toggle('active');
+});
+
+
+
