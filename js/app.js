738aaf3c32469aba7869b6f0c3e81de9d9071565
Alona Borealis <nadiadmitrov@gmail.com>
2026-03-30 14:46:25 +0300

    categories done. all first container is done without js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-const categoriesBtn = document.getElementById('categoriesBtn');
-const categories = document.getElementById('categories');
-const body = document.querySelector('body');
+const categoriesBtn = document.getElementById("categoriesBtn");
+const categories = document.getElementById("categories");
+const body = document.querySelector("body");
 
-categoriesBtn.addEventListener('click', () => {
-	categories.classList.toggle('active');
+categoriesBtn.addEventListener("click", () => {
+  categories.classList.toggle("active");
 });
-
-
-
